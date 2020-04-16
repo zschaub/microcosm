c6d374b5c72e37fb259a91366687c6bedd8db408
zschaub <bdzschau@gmail.com>
2020-04-16 12:42:56 -0400

    Added the ability to manually choose the language you want to be translated to

--- microcosm.py
+++ microcosm.py
@@ -6,14 +6,15 @@ app = Flask(__name__)
 
 # First element is language name, second is Google Translate language code, third is twilio Alice language code
 languages = [["Danish", 'da', 'da-DK'], ["German", 'de', 'de-DE'], ["English", 'en', 'en-US'],
-              ["Catalan", 'ca', 'ca-ES'], ["Spanish", 'es', 'es-ES'], ["Finish", 'fi', 'fi-FI'],
-              ["French", 'fr', 'fr-FR'], ["Italian", 'it', 'it-IT'], ["Japanese", 'ja', 'ja-JP'],
-              ["Korean", 'ko', 'ko-KR'], ["Norwegian", 'nb', 'nb-NO'], ["Dutch", 'nl', 'nl-NL'],
-              ["Polish", 'pl', 'pl-PL'], ["Portuguese", 'pt', 'pt-PT'], ["Russian", 'ru', 'ru-RU'],
-              ["Swedish", 'sv', 'sv-SE'], ["Chinese Manderin", 'zh-CN', 'zh-CN']]
+             ["Catalan", 'ca', 'ca-ES'], ["Spanish", 'es', 'es-ES'], ["Finish", 'fi', 'fi-FI'],
+             ["French", 'fr', 'fr-FR'], ["Italian", 'it', 'it-IT'], ["Japanese", 'ja', 'ja-JP'],
+             ["Korean", 'ko', 'ko-KR'], ["Norwegian", 'nb', 'nb-NO'], ["Dutch", 'nl', 'nl-NL'],
+             ["Polish", 'pl', 'pl-PL'], ["Portuguese", 'pt', 'pt-PT'], ["Russian", 'ru', 'ru-RU'],
+             ["Swedish", 'sv', 'sv-SE'], ["Chinese Mandarin", 'zh-CN', 'zh-CN']]
 
 source = 2
 destination = 2
+
 
 @app.route("/voice", methods=['GET', 'POST'])
 def voice():
@@ -23,7 +24,7 @@ def voice():
     gather = Gather(num_digits=1, action='/gather')
     gather.say(
         'For Spanish press 1, for Italian press 2, for German press 3, for French press 4, for Mandarin Chinese '
-        'press 5, for Japanese press 6', voice='Alice', language=languages[source][2])
+        'press 5, for Japanese press 6, to manually enter a language press 9', voice='Alice', language=languages[source][2])
     resp.append(gather)
 
     # If the user doesn't select an option, redirect them into a loop
@@ -63,15 +64,37 @@ def gather():
             resp.append(gather)
         elif choice == '5':
             destination = 16
-            gather.say('You have picked Mandarin Chinese, speak your message now', voice='Alice', language=languages[source][2])
+            gather.say('You have picked Mandarin Chinese, speak your message now', voice='Alice',
+                       language=languages[source][2])
             resp.append(gather)
         elif choice == '6':
             destination = 8
             gather.say('You have picked Japanese, speak your message now', voice='Alice', language=languages[source][2])
             resp.append(gather)
+        elif choice == '9':
+            gather = Gather(input='speech', action='/language')
+            gather.say('Please say what language you want to use', voice='Alice', language=languages[source][2])
+            resp.append(gather)
         else:
             resp.say("Sorry, I don't understand that choice.", voice='Alice', language=languages[source][2])
 
+    return str(resp)
+
+
+@app.route("/language", methods=['GET', 'POST'])
+def manual_language():
+    resp = VoiceResponse()
+    # print(request.values)
+    user_speech = request.form['SpeechResult']
+    for i in range(len(languages)):
+        if languages[i][0] == user_speech:
+            gather = Gather(input='speech', action='/translate')
+            gather.say('You have picked ' + languages[i][0] + ' speak your message now', voice='Alice', language=languages[source][2])
+            global destination
+            destination = i
+            resp.append(gather)
+            return str(resp)
+    resp.say(user_speech + ' is not a language I can help you with, please try again', voice='Alice', language=languages[source][2])
     return str(resp)
 
 
@@ -80,10 +103,10 @@ def translate():
     translator = Translator()
     resp = VoiceResponse()
     # print(request.values)
-    userSpeech = request.form['SpeechResult']
-    x = translator.translate(userSpeech, src=languages[source][1], dest=languages[destination][1])
-    translatedText = x.text
-    resp.say(translatedText, voice='Alice', language=languages[destination][2])
+    user_speech = request.form['SpeechResult']
+    x = translator.translate(user_speech, src=languages[source][1], dest=languages[destination][1])
+    translated_text = x.text
+    resp.say(translated_text, voice='Alice', language=languages[destination][2])
     return str(resp)
 
 
